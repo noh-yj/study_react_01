b7bb2f7f979187087badab9a2964fd88a8ab4017
noh-yj <goky456@gmail.com>
2021-03-20 11:46:13 +0900

    homework upload

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,7 +1,7 @@
 // import './App.css';
 import React from 'react';
-// import Start from './Start';
-// import Score from './Score';
+import Start from './Start';
+import Score from './Score';
 import Quiz from './Quiz';
 
 class App extends React.Component {
@@ -10,6 +10,25 @@ class App extends React.Component {
 
     this.state = {
       name: '스파르타 코딩 클럽',
+      page: 'quiz',
+      list: [
+        {
+          question: '르탄이는 1살이다',
+          answer: 'O',
+        },
+        {
+          question: '르탄이는 2살이다',
+          answer: 'O',
+        },
+        {
+          question: '르탄이는 3살이다',
+          answer: 'O',
+        },
+        {
+          question: '르탄이는 4살이다',
+          answer: 'O',
+        },
+      ],
       scoreMsg: '이 정도면 아주 친한 친구 사이! 앞으로도 더 친하게 지내요! :)',
     };
   }
@@ -17,9 +36,11 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        {/* <Start name={this.state.name} /> */}
-        {/* <Score name={this.state.name} msg={this.state.scoreMsg} /> */}
-        <Quiz />
+        {this.state.page === 'quiz' && <Quiz list={this.state.list} />}
+        {this.state.page === 'start' && <Start name={this.state.name} />}
+        {this.state.page === 'score' && (
+          <Score name={this.state.name} msg={this.state.scoreMsg} />
+        )}
       </div>
     );
   }
